d01c5f3d34644e3348bf7181ba06a05b689b22fb
AkhilMah14 <akhil@maheeps.net>
2023-10-04 17:49:22 -0400

    V1 of personal website

---
+++ personal-website/src/components/AboutSection.tsx
@@ -0,0 +1,81 @@
+import React from "react"
+import Image from "next/image"
+
+const skills = [
+  { skill: "Solidity" },
+  { skill: "Java" },
+  { skill: "Python" },
+  { skill: "C" },
+  { skill: "Assembly" },
+  { skill: "Swift" },
+  { skill: "HTML/CSS/Javascript" },
+  { skill: "Next.js" },
+  { skill: "Flask" },
+  { skill: "Docker" },
+  { skill: "Redis" },
+  { skill: "Jenkins" },
+  { skill: "Tailwind CSS" },
+  { skill: "Git" },
+  { skill: "Web3 Tooling: Alchemy, Hardhat, Waffle, GETH" },
+]
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              I love building things people use. The disruptive and nascent nature of blockchain captivate me, making it my field of exploration.
+            </p>
+            <br />
+            <p>
+              I'm currently pursuing a double major in Computer Science and Economics at the University of Virginia.
+            </p>
+            <br />
+            <p>
+              Besides being a crypto degen or nerding out on the latest Web3 infrastructure I lift weights and manage clients as the Director of External Relations @ <a href="https://vcguva.com/" className="text-teal-600">Virginia Consulting Group.</a> 
+            </p>
+            <br />
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <div className="relative mt-8 md:mt-0 flex flex-col items-center">
+                <Image
+                    src="/Web3License.png"
+                    alt=""
+                    width={325}
+                    height={325}
+                    className="rounded-full mt-10 mx-auto md:mx-0"
+                />
+                <p className="text-center text-gray-600 mt-2">My Car 😁</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
---
+++ personal-website/src/components/ProjectsSection.tsx
@@ -0,0 +1,86 @@
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+
+const projects = [
+  {
+    name: "Focuscoin (WIP)",
+    description:
+      "Focuscoin is a iOS dApp(Decentralized App) that rewards engagement in the study niche. Coins can be spent at study-related partners(Ex:] Speechify) in marketplace.",
+    image: "/FocusCoin.png",
+    link: "https://focuscoin.vercel.app/",
+  },
+  {
+    name: "Pare.",
+    description: "Pare is a iOS dApp that enables food pickup at the lowest prices and highest rewards. Backend payments are all done on the blockchain with 0% transaction fees, meaning money previously gone to middlemen goes back to users. Incentivizing recurring-revenue for partner restaurants.",
+    image: "/Pare.png",
+    link: "https://apps.apple.com/us/app/pare/id6446803674",
+  },
+  {
+    name: "MealX",
+    description:
+      "MealX is a iOS app to buy and sell meal exchanges @ University of Virginia. Not operational due to being shut down in violation of UVA's dining agreement.",
+    image: "/MealX.png",
+    github: "https://github.com/akhilrajEth/MealX",
+    link: "https://github.com/akhilrajEth/MealX",
+  },
+]
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                    <div className=" md:w-1/2">
+                    <Link href={project.link}>
+                        <Image
+                        src={project.image}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                        />
+                    </Link>
+                    </div>
+                    <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                        {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                        { project.name === "MealX" ? (
+                            <Link href={project.github} target="_blank">
+                                <BsGithub
+                                size={30}
+                                className="hover:-translate-y-1 transition-transform cursor-pointer"
+                                />
+                            </Link>)
+                        : (<Link href={project.link} target="_blank">
+                            <BsArrowUpRightSquare
+                                size={30}
+                                className="hover:-translate-y-1 transition-transform cursor-pointer"
+                            />
+                            </Link>)
+                        }
+                    </div>
+                    </div>
+                </div>
+            </div>
+          )
+        })}
+        
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsSection
